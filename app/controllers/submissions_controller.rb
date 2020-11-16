48793350c1b5ea05d3e9fd259d191c7904892b0d
Morrism1 <morrismontana0@gmail.com>
2020-11-16 14:32:26 +0200

    Refactor submissions controller to use current_user

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -15,7 +15,7 @@ class SubmissionsController < ApplicationController
 
   # GET /submissions/new
   def new
-    @submission = current_user.submissions.new
+    @submission = current_user.submissions.build
   end
 
   # GET /submissions/1/edit
@@ -24,7 +24,7 @@ class SubmissionsController < ApplicationController
   # POST /submissions
   # POST /submissions.json
   def create
-    @submission = current_user.submissions.new(submission_params)
+    @submission = current_user.submissions.build(submission_params)
 
     respond_to do |format|
       if @submission.save
