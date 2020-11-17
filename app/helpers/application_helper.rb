cdc206c078070b26bbca86d48d1160804ccc6927
Morrism1 <morrismontana0@gmail.com>
2020-11-17 14:23:17 +0200

    Add helper to handle inline svg

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+    def render_svg(name, styles: 'fill-current text-gray-400', title:nil)
+      filename = "#{name}.svg"
+      title ||= name.underscore.humanize
+      inline_svg_tag(filename, aria: true, nocomment: true, title: title, class: styles)
+    end
 end
