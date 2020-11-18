31a866ba87578ab4305da694d38d1da57a9c3f46
Morrism1 <morrismontana0@gmail.com>
2020-11-18 14:39:52 +0200

    Add user reference to communities table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_18_122330) do
+ActiveRecord::Schema.define(version: 2020_11_18_123817) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,7 +22,9 @@ ActiveRecord::Schema.define(version: 2020_11_18_122330) do
     t.text "sidebar"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
     t.index ["name"], name: "index_communities_on_name", unique: true
+    t.index ["user_id"], name: "index_communities_on_user_id"
   end
 
   create_table "submissions", force: :cascade do |t|
@@ -53,6 +55,7 @@ ActiveRecord::Schema.define(version: 2020_11_18_122330) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "communities", "users"
   add_foreign_key "submissions", "communities"
   add_foreign_key "submissions", "users"
 end
