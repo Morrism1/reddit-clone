7786c401d4b315fc77787518297804b47eb66d74
Morrism1 <morrismontana0@gmail.com>
2020-11-18 14:15:53 +0200

    Add associations between models

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :submissions, dependent: :destroy
+  has_many :communities
 
   validates_uniqueness_of :username
   validates_presence_of :username
