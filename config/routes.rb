fcf1b81295817ddb63b4ab9ba5a21c4a2d486ba4
Morrism1 <morrismontana0@gmail.com>
2020-11-18 14:11:17 +0200

    Add community scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :communities
   resources :submissions
   devise_for :users
   resources :users, only: [:show], as: "profile"
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_17_125136) do
+ActiveRecord::Schema.define(version: 2020_11_18_120748) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "communities", force: :cascade do |t|
+    t.string "name"
+    t.string "title"
+    t.text "description"
+    t.text "sidebar"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["name"], name: "index_communities_on_name", unique: true
+  end
 
   create_table "submissions", force: :cascade do |t|
     t.string "title"
